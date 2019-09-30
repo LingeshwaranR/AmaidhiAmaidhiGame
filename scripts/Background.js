6f2128958db5a079adaa5ada9cf33ae8e7f8fbae
LingeshwaranR <lingeshwaran001@gmail.com>
2019-09-30 09:42:45 +0530

    no message

--- scripts/Background.js
+++
@@ -1,5 +0,0 @@
-position = 0;
-var intervalBackground = setInterval(function() {
-    position -= .15;
-    $(".background-move").css({ "background-position": +position + "px 0px" })
-}, 0);
--- scripts/Platform.js
+++
@@ -1,5 +0,0 @@
-position = 0;
-var intervalPlatform = setInterval(function() {
-    position -= 1;
-    $(".platform").css({ "background-position": +position + "px 0px" })
-}, 10);
